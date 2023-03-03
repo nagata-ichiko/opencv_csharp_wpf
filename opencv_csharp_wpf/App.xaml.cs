b502ef3f30acc0b96b94b3b99ae00279042c798e
nagata-ichiko <nagataichiko2249@gmail.com>
2023-03-04 00:19:27 +0900

    プロジェクト作成

---
+++ opencv_csharp_wpf/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using opencv_csharp_wpf.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace opencv_csharp_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
